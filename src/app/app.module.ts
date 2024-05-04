9bead3ee3a3c1029ab4caa44485798895e526e9a
Mike Wasoski <miguelperez@aimtech.es>
2024-05-04 17:39:11 +0200

    Inputs y Outputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,10 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 
-
 @NgModule({
   declarations: [
     AppComponent,
-    NopagefoundComponent,    
+    NopagefoundComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,9 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
+  styleUrls: [ './progress.component.css'
   ]
 })
 export class ProgressComponent {
+  progreso1: number = 22;
+  progreso2: number = 42;
 
+  get getProgreso1(){
+    return `${ this.progreso1 }%`
+  }
+
+  get getProgreso2(){
+    return `${ this.progreso2 }%`
+  }
 }
